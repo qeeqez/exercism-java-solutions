699cd49f969a37f678232cb06c136e31501655f1
qeeqez <qeeqez@gmail.com>
2023-01-29 23:01:22 +0300

    Solve IsogramChecker using String methods

--- isogram/src/main/java/IsogramChecker.java
+++ isogram/src/main/java/IsogramChecker.java
@@ -1,18 +1,12 @@
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        int[] chars = new int[256];
+        phrase = phrase
+                .replace("-", "")
+                .replace(" ", "")
+                .toLowerCase();
 
-        for (char c : phrase.toCharArray()) {
-            if (Character.isLetter(c)) {
-                c = Character.toLowerCase(c);
-                int index = c - 'a';
-                chars[index]++;
-                if (chars[index] > 1) return false;
-            }
-        }
-
-        return true;
+        return phrase.chars().distinct().count() == phrase.length();
     }
 
 }
