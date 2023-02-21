06b8424b244d2dc19f000cd44897ea13ed2b9f9d
qeeqez <qeeqez@gmail.com>
2023-02-21 19:07:57 +0300

    Solve Saddle Points

---
+++ saddle-points/src/main/java/Matrix.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+class Matrix {
+
+    private final List<List<Integer>> values;
+
+    Matrix(List<List<Integer>> values) {
+        this.values = values;
+    }
+
+    Set<MatrixCoordinate> getSaddlePoints() {
+        Set<MatrixCoordinate> saddlePoints = new HashSet<>();
+
+        for (int i = 0; i < values.size(); i++) {
+            int maxInRow = maxInRow(i);
+
+            for (int j = 0; j < values.get(i).size(); j++) {
+                if (values.get(i).get(j) == maxInRow && values.get(i).get(j) == minInCol(j)) {
+                    saddlePoints.add(new MatrixCoordinate(i + 1, j + 1));
+                }
+            }
+        }
+
+        return saddlePoints;
+    }
+
+    private int maxInRow(int row) {
+        return values.get(row).parallelStream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
+    private int minInCol(int column) {
+        AtomicInteger minInCol = new AtomicInteger(Integer.MAX_VALUE);
+        values.parallelStream().forEach(row -> {
+            int value = row.get(column);
+            if (minInCol.get() > value) minInCol.set(value);
+        });
+        return minInCol.get();
+    }
+}
