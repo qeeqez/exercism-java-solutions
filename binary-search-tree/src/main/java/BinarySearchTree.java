cf458f87b1a8a89722f29b6a5059c02734f09fbd
qeeqez <qeeqez@gmail.com>
2023-02-25 02:04:13 +0300

    Solve Binary Search Tree

---
+++ binary-search-tree/src/main/java/BinarySearchTree.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+class BinarySearchTree<T extends Comparable<T>> {
+
+    private Node<T> root = null;
+
+    void insert(T value) {
+        root = insert(root, value);
+    }
+
+    private Node<T> insert(Node<T> node, T value) {
+        if (node == null) return new Node<>(value);
+
+        int compare = value.compareTo(node.getData());
+
+        if (compare > 0) node.right = insert(node.right, value);
+        else node.left = insert(node.left, value);
+
+        return node;
+    }
+
+    public List<T> getAsSortedList() {
+        return get(root);
+    }
+
+    private List<T> get(Node<T> pos) {
+        if (pos == null) return new ArrayList<>();
+        List<T> res = get(pos.getLeft());
+        res.add(pos.getData());
+        res.addAll(get(pos.getRight()));
+        return res;
+    }
+
+    public List<T> getAsLevelOrderList() {
+        List<T> res = new ArrayList<>();
+
+        Queue<Node<T>> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node<T> pos = queue.poll();
+            res.add(pos.getData());
+            if (pos.getLeft() != null) queue.add(pos.getLeft());
+            if (pos.getRight() != null) queue.add(pos.getRight());
+        }
+        return res;
+    }
+
+    public Node<T> getRoot() {
+        return root;
+    }
+
+    static class Node<T> {
+
+        private final T data;
+        private Node<T> left;
+        private Node<T> right;
+
+        public Node(T value) {
+            data = value;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public Node<T> getLeft() {
+            return left;
+        }
+
+        public Node<T> getRight() {
+            return right;
+        }
+
+    }
+}
