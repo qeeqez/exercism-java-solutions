4c3e1a8734b559f7093a6d4defd416efa9e930ad
qeeqez <qeeqez@gmail.com>
2024-01-12 21:47:20 +0300

    Solve Wizards and Warriors 2

--- wizards-and-warriors-2/src/main/java/GameMaster.java
+++ wizards-and-warriors-2/src/main/java/GameMaster.java
@@ -1,12 +1,31 @@
 public class GameMaster {
 
-    // TODO: define a 'describe' method that returns a description of a Character
+    public String describe(Character character) {
+        return String.format("You're a level %d %s with %d hit points.",
+                             character.getLevel(),
+                             character.getCharacterClass(),
+                             character.getHitPoints()
+        );
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Destination
+    public String describe(Destination destination) {
+        return String.format("You've arrived at %s, which has %s inhabitants.",
+                             destination.getName(),
+                             destination.getInhabitants()
+        );
+    }
 
-    // TODO: define a 'describe' method that returns a description of a TravelMethod
+    public String describe(TravelMethod travel) {
+        String travelStr = travel == TravelMethod.WALKING ? "by walking" : "on horseback";
+        return String.format("You're traveling to your destination %s.", travelStr);
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Character, Destination and TravelMethod
+    public String describe(Character character, Destination destination, TravelMethod travel) {
+        return describe(character) + " " + describe(travel) + " " + describe(destination);
+    }
 
-    // TODO: define a 'describe' method that returns a description of a Character and Destination
+    public String describe(Character character, Destination destination) {
+        return describe(character) + " " + describe(TravelMethod.WALKING) + " " + describe(destination);
+    }
+
 }
