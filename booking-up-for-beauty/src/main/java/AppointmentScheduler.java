278c7b1eddbf7dd654272561ab335cb26528420f
qeeqez <qeeqez@gmail.com>
2024-01-12 20:33:15 +0300

    Alternative more compact solution for Booking Up For Beauty

--- booking-up-for-beauty/src/main/java/AppointmentScheduler.java
+++ booking-up-for-beauty/src/main/java/AppointmentScheduler.java
@@ -1,11 +1,7 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.format.FormatStyle;
-import java.util.Locale;
 
 class AppointmentScheduler {
     public LocalDateTime schedule(String appointmentDateDescription) {
@@ -24,17 +20,8 @@ class AppointmentScheduler {
 
     // return "You have an appointment on Friday, March 29, 2019, at 3:00 PM."
     public String getDescription(LocalDateTime appointmentDate) {
-        LocalDate date = appointmentDate.toLocalDate();
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy");
-
-        LocalTime time = appointmentDate.toLocalTime();
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("h:mm a");
-
-        return "You have an appointment on " +
-                date.format(dateFormatter) +
-                ", at " +
-                time.format(timeFormatter) +
-                ".";
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy, 'at' h:mm a'.'");
+        return "You have an appointment on " + appointmentDate.format(dateFormatter);
     }
 
     public LocalDate getAnniversaryDate() {
