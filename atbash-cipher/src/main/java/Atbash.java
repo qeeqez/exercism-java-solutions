3c4cf9bba07842bd4d010997d2ce94fd75ccf6e6
Maksim Sasnouski <qeeqez@gmail.com>
2024-03-25 17:33:02 +0300

    Solve Atbash Cipher

--- atbash-cipher/src/main/java/Atbash.java
+++ atbash-cipher/src/main/java/Atbash.java
@@ -1,11 +1,40 @@
 class Atbash {
 
     String encode(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        input = clearString(input);
+
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            if (i > 0 && i % 5 == 0) output.append(" ");
+            output.append(encodeDecode(input.charAt(i)));
+        }
+
+        return output.toString();
     }
 
     String decode(String input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        input = clearString(input);
+
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            output.append(encodeDecode(input.charAt(i)));
+        }
+
+        return output.toString();
     }
 
+    private String clearString(String input) {
+        return input
+                .replaceAll("[^A-Za-z1-9]+", "")
+                .toLowerCase();
+    }
+
+    private char encodeDecode(char c) {
+        if (isNumber(c)) return c;
+        return (char) ('z' - c + 'a');
+    }
+
+    private boolean isNumber(char c) {
+        return c >= 48 && c <= 57;
+    }
 }
