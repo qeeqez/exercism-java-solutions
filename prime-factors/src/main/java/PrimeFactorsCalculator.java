c4199f05ef05530efb5bc38ed2866ee8914ede52
Maksim Sasnouski <qeeqez@gmail.com>
2024-03-28 14:48:17 +0300

    Add Prime Factors

---
+++ prime-factors/src/main/java/PrimeFactorsCalculator.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+class PrimeFactorsCalculator {
+
+    List<Long> calculatePrimeFactorsOf(long number) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
