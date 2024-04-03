6ca424e80a246715d289dcde540fe0e727399a4b
Maksim Sasnouski <qeeqez@gmail.com>
2024-04-03 15:48:48 +0300

    Sublist: implement Equal case

--- sublist/src/main/java/RelationshipComputer.java
+++ sublist/src/main/java/RelationshipComputer.java
@@ -2,6 +2,8 @@ import java.util.List;
 
 class RelationshipComputer<T> {
     Relationship computeRelationship(List<T> firstList, List<T> secondList) {
-        throw new UnsupportedOperationException("Please implement the RelationshipComputer.computeRelationship() method.");
+        if(firstList.equals(secondList)) return Relationship.EQUAL;
+
+        return Relationship.UNEQUAL;
     }
 }
