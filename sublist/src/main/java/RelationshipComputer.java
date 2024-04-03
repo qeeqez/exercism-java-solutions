c5dfc06d30826ec659bc720377ce755d9e13c546
Maksim Sasnouski <qeeqez@gmail.com>
2024-04-03 16:05:28 +0300

    Solve Sublist manually

--- sublist/src/main/java/RelationshipComputer.java
+++ sublist/src/main/java/RelationshipComputer.java
@@ -2,8 +2,34 @@ import java.util.List;
 
 class RelationshipComputer<T> {
     Relationship computeRelationship(List<T> firstList, List<T> secondList) {
-        if(firstList.equals(secondList)) return Relationship.EQUAL;
+        if (firstList.equals(secondList)) return Relationship.EQUAL;
+
+        if (isSublist(firstList, secondList)) return Relationship.SUPERLIST;
+        if (isSublist(secondList, firstList)) return Relationship.SUBLIST;
 
         return Relationship.UNEQUAL;
+    }
+
+    private boolean isSublist(List<T> firstList, List<T> secondList) {
+        if (secondList.size() > firstList.size()) return false;
+        if (secondList.isEmpty()) return true;
+        if (secondList.size() == firstList.size()) return firstList.equals(secondList);
+
+        boolean isSublist = false;
+        for (int i = 0; i < firstList.size() - secondList.size() + 1; i++) {
+            int k = i;
+            for (T second : secondList) {
+                if (firstList.get(k).equals(second)) {
+                    k++;
+                    isSublist = true;
+                } else {
+                    isSublist = false;
+                    break;
+                }
+            }
+            if (isSublist) return true;
+        }
+
+        return false;
     }
 }
