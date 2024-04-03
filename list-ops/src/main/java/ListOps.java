7a7a5762ff63b5ce8fbe6909d37115b99939019c
Maksim Sasnouski <qeeqez@gmail.com>
2024-04-03 16:17:21 +0300

    Solve List Ops

--- list-ops/src/main/java/ListOps.java
+++ list-ops/src/main/java/ListOps.java
@@ -1,44 +1,45 @@
+import java.util.Collection;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 class ListOps {
 
     static <T> List<T> append(List<T> list1, List<T> list2) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return Stream.concat(list1.stream(), list2.stream()).toList();
     }
 
     static <T> List<T> concat(List<List<T>> listOfLists) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return listOfLists.stream().flatMap(Collection::stream).toList();
     }
 
     static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return list.stream().filter(predicate).toList();
     }
 
     static <T> int size(List<T> list) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return list.size();
     }
 
     static <T, U> List<U> map(List<T> list, Function<T, U> transform) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return list.stream().map(transform).toList();
     }
 
     static <T> List<T> reverse(List<T> list) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return list.stream().toList().reversed();
     }
 
     static <T, U> U foldLeft(List<T> list, U initial, BiFunction<U, T, U> f) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return list.stream().reduce(initial, f, (u1, u2) -> u1);
     }
 
     static <T, U> U foldRight(List<T> list, U initial, BiFunction<T, U, U> f) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return reverse(list).stream().reduce(initial, (u, t) -> f.apply(t, u), (u1, u2) -> u1);
     }
 
     private ListOps() {
-        // No instances.
     }
 
 }
